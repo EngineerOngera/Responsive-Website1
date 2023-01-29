1897c5befcef14e2f71bcb55deb1be02ee8a7b78
erickongera <engineerongera@gmail.com>
2023-01-29 19:12:13 +0300

    typing

---
+++ backup_main.js
@@ -0,0 +1,18 @@
+
+jQuery(document).ready(function(){
+"user strick"
+$('.slider').ripples({
+    dropRadius: 10,
+    perturbance: 0.01,
+    
+  });
+
+  $('.text').typed({
+    strings:["First sentence.", "Second sentence"],
+    typespeed:0,
+    loop:true
+
+
+  })
+
+})
